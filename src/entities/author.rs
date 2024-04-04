44d4e0f9477c5ca9700ed500d1a846cfdd870a64
Ayan Das <bvits@riseup.net>
2024-04-04 11:50:03 +0530

    bugfix: fix serialization/deserialization for author entity

--- src/entities/author.rs
+++ src/entities/author.rs
@@ -1,5 +1,9 @@
-use crate::entities::common::{Affiliation, AuthorIds, DehydratedInstitution, DehydratedConcept, SummaryStats, CountByYear};
-use serde::{Serialize, Deserialize};
+use crate::bakery::{Deflatable, Deflated, Deflation, Leavenable};
+use crate::entities::common::{
+    Affiliation, AuthorIds, CountByYear, DehydratedConcept, DehydratedInstitution, SummaryStats,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, Error as SerdeError};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Author {
@@ -19,4 +23,35 @@ pub struct Author {
     works_api_url: String,
     works_count: i32,
     x_concepts: Vec<DehydratedConcept>,
+}
+
+impl Author {
+    pub fn leaven<L: Leavenable>(input: L) -> Result<Self, SerdeError> {
+        L::leaven(input)
+    }
+}
+
+impl Deflatable for Author {
+    fn deflate(&self, format: Deflation) -> Result<Deflated, SerdeError> {
+        match format {
+            Deflation::ToString => serde_json::to_string(self).map(Deflated::String),
+            Deflation::ToJsonValue => serde_json::to_value(self).map(Deflated::JsonValue),
+            Deflation::ToByteArray => serde_json::to_vec(self).map(Deflated::ByteArray),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_author_idempotence_sugarred() {
+        crate::entity_idempotence_sugarred!(Author, "testdata/entities/author.json");
+    }
+
+    #[test]
+    fn test_author_idempotence_desugarred() {
+        crate::entity_idempotence_desugarred!(Author, "testdata/entities/author.json");
+    }
 }
