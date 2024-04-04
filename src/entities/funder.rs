ddbbe014be49d108becb4778d3ff416647de2211
Ayan Das <bvits@riseup.net>
2024-04-04 11:51:06 +0530

    bugfix: fix serialization/deserialization for funder entity

--- src/entities/funder.rs
+++ src/entities/funder.rs
@@ -1,5 +1,7 @@
-use serde::{Serialize, Deserialize};
+use crate::bakery::{Deflatable, Deflated, Deflation, Leavenable};
 use crate::entities::common::{CountsByYear, FunderIds, Role, SummaryStats};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, Error as SerdeError};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Funder {
@@ -20,4 +22,36 @@ pub struct Funder {
     summary_stats: SummaryStats,
     updated_date: String,
     works_count: i32,
+}
+
+impl Funder {
+    pub fn leaven<L: Leavenable>(input: L) -> Result<Self, SerdeError> {
+        L::leaven(input)
+    }
+}
+
+impl Deflatable for Funder {
+    fn deflate(&self, format: Deflation) -> Result<Deflated, SerdeError> {
+        match format {
+            Deflation::ToString => serde_json::to_string(self).map(Deflated::String),
+            Deflation::ToJsonValue => serde_json::to_value(self).map(Deflated::JsonValue),
+            Deflation::ToByteArray => serde_json::to_vec(self).map(Deflated::ByteArray),
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_funder_idempotence_sugarred() {
+        crate::entity_idempotence_sugarred!(Funder, "testdata/entities/funder.json");
+    }
+
+    #[test]
+    fn test_funder_idempotence_desugarred() {
+        crate::entity_idempotence_desugarred!(Funder, "testdata/entities/funder.json");
+    }
 }
