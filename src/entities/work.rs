3b537433e3da73297987de6e7c373dcfd3cb28d4
Ayan Das <bvits@riseup.net>
2024-04-04 11:52:09 +0530

    bugfix: fix serialization/deserialization for work entity

--- src/entities/work.rs
+++ src/entities/work.rs
@@ -1,5 +1,10 @@
-use crate::entities::common::{Authorship, Apc, Location, Biblio, YearCount, Concept, Grant, Keyword, MeshTag, WorkIds, OpenAccess, License, Sdg, Topic, WorkType};
+use crate::bakery::{Deflatable, Deflated, Deflation, Leavenable};
+use crate::entities::common::{
+    Apc, Authorship, Biblio, Concept, Grant, Keyword, Location, MeshTag, OpenAccess,
+    PercentileYear, Sdg, Topic, WorkIds, WorkType, YearCount,
+};
 use serde::{Deserialize, Serialize};
+use serde_json::{self, Error as SerdeError};
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -12,6 +17,7 @@ pub struct Work {
     biblio: Option<Biblio>,
     cited_by_api_url: String,
     cited_by_count: usize,
+    cited_by_percentile_year: Option<PercentileYear>,
     concepts: Vec<Concept>,
     corresponding_author_ids: Vec<String>,
     corresponding_institution_ids: Vec<String>,
@@ -31,7 +37,6 @@ pub struct Work {
     is_retracted: bool,
     keywords: Option<Vec<Keyword>>,
     language: Option<String>,
-    license: Option<License>,
     locations: Vec<Location>,
     locations_count: usize,
     mesh: Option<Vec<MeshTag>>,
@@ -42,12 +47,46 @@ pub struct Work {
     publication_date: Option<String>,
     publication_year: Option<usize>,
     referenced_works: Option<Vec<String>>,
+    referenced_works_count: Option<usize>,
     related_works: Option<Vec<String>>,
     sustainable_development_goals: Option<Vec<Sdg>>,
-    topics: Option<Vec<Topic>>,
     title: String,
+    topics: Option<Vec<Topic>>,
     #[serde(rename = "type")]
     work_type: WorkType,
     type_crossref: Option<String>,
     updated_date: Option<String>,
+    #[serde(default)]
+    pub versions: Vec<String>,
+}
+
+impl Work {
+    pub fn leaven<L: Leavenable>(input: L) -> Result<Self, SerdeError> {
+        L::leaven(input)
+    }
+}
+
+impl Deflatable for Work {
+    fn deflate(&self, format: Deflation) -> Result<Deflated, SerdeError> {
+        match format {
+            Deflation::ToString => serde_json::to_string(self).map(Deflated::String),
+            Deflation::ToJsonValue => serde_json::to_value(self).map(Deflated::JsonValue),
+            Deflation::ToByteArray => serde_json::to_vec(self).map(Deflated::ByteArray),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_work_idempotence_sugarred() {
+        crate::entity_idempotence_sugarred!(Work, "testdata/entities/work.json");
+    }
+
+    #[test]
+    fn test_work_idempotence_desugarred() {
+        crate::entity_idempotence_desugarred!(Work, "testdata/entities/work.json");
+    }
 }
