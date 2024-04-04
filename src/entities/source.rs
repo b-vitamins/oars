7867e58025ab87d873329150e8ca7708f005f7cc
Ayan Das <bvits@riseup.net>
2024-04-04 11:51:40 +0530

    bugfix: fix serialization/deserialization for source entity

--- src/entities/source.rs
+++ src/entities/source.rs
@@ -1,9 +1,13 @@
-use crate::entities::common::{ApcPrice, CountsByYear, SourceIds, Society, SummaryStats, SourceType, DehydratedConcept};
-use serde::{Serialize, Deserialize};
+use crate::bakery::{Deflatable, Deflated, Deflation, Leavenable};
+use crate::entities::common::{
+    ApcPrice, CountsByYear, DehydratedConcept, Society, SourceIds, SourceType, SummaryStats,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, Error as SerdeError};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Source {
-    abbreviated_title: String,
+    abbreviated_title: Option<String>,
     alternate_titles: Vec<String>,
     apc_prices: Vec<ApcPrice>,
     apc_usd: Option<i32>,
@@ -30,4 +34,37 @@ pub struct Source {
     works_api_url: String,
     works_count: i32,
     x_concepts: Vec<DehydratedConcept>,
+}
+
+impl Source {
+    pub fn leaven<L: Leavenable>(input: L) -> Result<Self, SerdeError> {
+        L::leaven(input)
+    }
+}
+
+impl Deflatable for Source {
+    fn deflate(&self, format: Deflation) -> Result<Deflated, SerdeError> {
+        match format {
+            Deflation::ToString => serde_json::to_string(self).map(Deflated::String),
+            Deflation::ToJsonValue => serde_json::to_value(self).map(Deflated::JsonValue),
+            Deflation::ToByteArray => serde_json::to_vec(self).map(Deflated::ByteArray),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Source tests
+    #[test]
+    fn test_source_idempotence_sugarred() {
+        crate::entity_idempotence_sugarred!(Source, "testdata/entities/source.json");
+    }
+
+    #[test]
+    fn test_source_idempotence_desugarred() {
+        crate::entity_idempotence_desugarred!(Source, "testdata/entities/source.json");
+    }
+
 }
