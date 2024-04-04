7c7ff72a74173ae9e4294bc6907be73bcf953ce4
Ayan Das <bvits@riseup.net>
2024-04-04 11:51:54 +0530

    bugfix: fix serialization/deserialization for publisher entity

--- src/entities/publisher.rs
+++ src/entities/publisher.rs
@@ -1,5 +1,7 @@
-use serde::{Serialize, Deserialize};
+use crate::bakery::{Deflatable, Deflated, Deflation, Leavenable};
 use crate::entities::common::{CountByYear, PublisherIds, Role, SummaryStats};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, Error as SerdeError};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Publisher {
@@ -10,6 +12,7 @@ pub struct Publisher {
     created_date: String,
     display_name: String,
     hierarchy_level: i32,
+    homepage_url: Option<String>,
     id: String,
     ids: PublisherIds,
     image_thumbnail_url: String,
@@ -21,4 +24,34 @@ pub struct Publisher {
     summary_stats: SummaryStats,
     updated_date: String,
     works_count: i32,
+}
+
+impl Publisher {
+    pub fn leaven<L: Leavenable>(input: L) -> Result<Self, SerdeError> {
+        L::leaven(input)
+    }
+}
+
+impl Deflatable for Publisher {
+    fn deflate(&self, format: Deflation) -> Result<Deflated, SerdeError> {
+        match format {
+            Deflation::ToString => serde_json::to_string(self).map(Deflated::String),
+            Deflation::ToJsonValue => serde_json::to_value(self).map(Deflated::JsonValue),
+            Deflation::ToByteArray => serde_json::to_vec(self).map(Deflated::ByteArray),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_publisher_idempotence_sugarred() {
+        crate::entity_idempotence_sugarred!(Publisher, "testdata/entities/publisher.json");
+    }
+
+    #[test]
+    fn test_publisher_idempotence_desugarred() {
+        crate::entity_idempotence_desugarred!(Publisher, "testdata/entities/publisher.json");
+    }
 }
