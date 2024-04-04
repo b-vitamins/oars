75ca62b108b65d50f2b56c6b18a964e0155e9d54
Ayan Das <bvits@riseup.net>
2024-04-04 11:52:20 +0530

    bugfix: fix serialization/deserialization for topic entity

--- src/entities/topic.rs
+++ src/entities/topic.rs
@@ -1,5 +1,7 @@
-use crate::entities::common::{Domain, Field, TopicIds, Subfield};
-use serde::{Serialize, Deserialize};
+use crate::bakery::{Deflatable, Deflated, Deflation, Leavenable};
+use crate::entities::common::{Domain, Field, Subfield, TopicIds};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, Error as SerdeError};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Topic {
@@ -12,7 +14,40 @@ pub struct Topic {
     id: String,
     ids: TopicIds,
     keywords: Vec<String>,
+    #[serde(default)]
+    siblings: Option<Vec<String>>,
     subfield: Subfield,
     updated_date: String,
     works_count: i32,
+}
+
+impl Topic {
+    pub fn leaven<L: Leavenable>(input: L) -> Result<Self, SerdeError> {
+        L::leaven(input)
+    }
+}
+
+impl Deflatable for Topic {
+    fn deflate(&self, format: Deflation) -> Result<Deflated, SerdeError> {
+        match format {
+            Deflation::ToString => serde_json::to_string(self).map(Deflated::String),
+            Deflation::ToJsonValue => serde_json::to_value(self).map(Deflated::JsonValue),
+            Deflation::ToByteArray => serde_json::to_vec(self).map(Deflated::ByteArray),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_author_idempotence_sugarred() {
+        crate::entity_idempotence_sugarred!(Topic, "testdata/entities/topic.json");
+    }
+
+    #[test]
+    fn test_author_idempotence_desugarred() {
+        crate::entity_idempotence_desugarred!(Topic, "testdata/entities/topic.json");
+    }
 }
